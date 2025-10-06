370da045f8fc82e876b798c15803d6e756f94a3b
Müzemmil Temel <temel.muzemmil@gmail.com>
2025-10-06 12:31:34 +0200

    ajout test d'essai pour django

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+# ici c'est juste pour tester github actions
+import pytest
+
+def test_dummy():
+    assert True
+
+@pytest.mark.skip(reason="example skip")
+def test_skipped():
+    assert False
+
